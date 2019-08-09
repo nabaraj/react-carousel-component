1e79ba7f41809aa85cae548b244e78ebba2e1594
ine12362324 <nabaraj.saha@tesco.com>
2019-08-09 19:21:36 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          //"style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  }
+};
