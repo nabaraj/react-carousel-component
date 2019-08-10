0dc19487858da2a865417b9dd1ba229fad51341b
nabaraj <nabaraj2k@gmail.com>
2019-08-10 12:59:04 +0530

    added css fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
+var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "index.css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    })
+  ],
   output: {
     path: path.join(__dirname, "build"),
     filename: "index.bundle.js"
@@ -15,6 +26,7 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         use: [
+          MiniCssExtractPlugin.loader,
           //"style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
           "sass-loader" // compiles Sass to CSS, using Node Sass by default
